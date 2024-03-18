a36298495106f0e5375b83409103a6656a355dd9
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-03-18 22:32:36 +0530

    created dummy product list and list rendering

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,33 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY = {
+  1: {
+    title: "Test",
+    price: 6,
+    description: "This is first product - amazing!",
+  },
+  2 : {
+    title: "another",
+    price: 10,
+    description: "This is second product - amazing!"
+  }
+};
 
 const Products = (props) => {
+
+  const productList = Object.keys(DUMMY).map((id, index)=>{
+    return (
+      <li key={index}>
+        <ProductItem title={DUMMY[id].title} price={DUMMY[id].price} description={DUMMY[id].description} />
+      </li>
+    )
+  })
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {productList}
       </ul>
     </section>
   );
